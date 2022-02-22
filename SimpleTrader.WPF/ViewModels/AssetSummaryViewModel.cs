0a7cbb630ff31927b2bd307be26c495711a1596f
Samad Hatsijev <s.hatsijev@hotmail.com>
2022-02-22 20:59:48 +0100

    Asset & View Style Update

--- SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -9,16 +9,16 @@ namespace SimpleTrader.WPF.ViewModels
     {
         private readonly AssetStore _assetStore;
 
-        private readonly ObservableCollection<AssetViewModel> _assets;
+        private readonly ObservableCollection<AssetViewModel> _topAassets;
 
         public double AccountBalance => _assetStore.AccountBalance;
-        public IEnumerable<AssetViewModel> Assets => _assets;
+        public IEnumerable<AssetViewModel> TopAssets => _topAassets;
 
         public AssetSummaryViewModel(AssetStore assetStore)
         {
             _assetStore = assetStore;
 
-            _assets = new ObservableCollection<AssetViewModel>();
+            _topAassets = new ObservableCollection<AssetViewModel>();
 
             _assetStore.StateChanged += AssetStore_StateChanged;
 
@@ -36,13 +36,15 @@ namespace SimpleTrader.WPF.ViewModels
             IEnumerable<AssetViewModel> assetViewModels = _assetStore.AssetTransactions
                 .GroupBy(t => t.Asset.Symbol)
                 .Select(g => new AssetViewModel(g.Key, g.Sum(a => a.IsPurchase ? a.Shares : -a.Shares)))
-                .Where(a => a.Shares > 0);
+                .Where(a => a.Shares > 0)
+                .OrderByDescending(a => a.Shares)
+                .Take(3);
 
-            _assets.Clear();
+            _topAassets.Clear();
 
             foreach (AssetViewModel assetViewModel in assetViewModels)
             {
-                _assets.Add(assetViewModel);
+                _topAassets.Add(assetViewModel);
             }
         }
     }
